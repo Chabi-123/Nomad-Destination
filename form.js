c3450aab1fa03c44bd1a8510755c922c10804c44
Chabi-123 <168256568+Chabi-123@users.noreply.github.com>
2024-05-29 17:29:36 +0600

    Add files via upload

---
+++ form.js
@@ -0,0 +1,9 @@
+let input = document.querySelector("input");
+function submit(){
+    if(input.value==""){
+        alert("please fill every details")
+    }
+    else(
+        alert("you have successfully submitted your information")
+    )
+}
---
+++ script.js
@@ -0,0 +1,11 @@
+function showSidebar(){
+    const sidebar = document.getElementById('headerTwo')
+    sidebar.style.display = 'flex'
+    sidebar.style.cursor = 'pointer'
+    }
+
+function closeSidebar(){
+    const sidebar = document.getElementById('headerTwo')
+    sidebar.style.display = "none"
+    sidebar.style.cursor = 'pointer'
+}
